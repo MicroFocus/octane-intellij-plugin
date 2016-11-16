10233006abc93f5f2d2936256afe03daacc0aa81
Andras Toth <atoth@hpe.com>
2016-11-16 14:58:12 +0200

    Password is now encrypted in the Intellij password store, no longer stored as text in an .xml file

--- src/main/java/com/hpe/adm/octane/ideplugins/intellij/settings/IdePersistentConnectionSettingsProvider.java
+++ src/main/java/com/hpe/adm/octane/ideplugins/intellij/settings/IdePersistentConnectionSettingsProvider.java
@@ -2,9 +2,14 @@ package com.hpe.adm.octane.ideplugins.intellij.settings;
 
 import com.hpe.adm.octane.ideplugins.services.connection.ConnectionSettings;
 import com.hpe.adm.octane.ideplugins.services.connection.ConnectionSettingsProvider;
+import com.intellij.ide.passwordSafe.PasswordSafe;
+import com.intellij.ide.passwordSafe.PasswordSafeException;
 import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
+import com.intellij.openapi.util.text.StringUtil;
+import org.jdom.Element;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 @State(
@@ -14,19 +19,63 @@ import org.jetbrains.annotations.Nullable;
                         file = "$APP_CONFIG$/octane_connection_settings.xml"
                 )}
 )
-public class IdePersistentConnectionSettingsProvider implements PersistentStateComponent<ConnectionSettings>, ConnectionSettingsProvider {
+public class IdePersistentConnectionSettingsProvider implements PersistentStateComponent<Element>, ConnectionSettingsProvider {
+
+    private static final String OCTANE_PASSWORD_KEY = "OCTANE_PASSWORD_KEY";
+
+    private static final String CONNECTION_SETTINGS_TAG = "ConnectionSettings";
+    private static final String URL_TAG = "Url";
+    private static final String SHARED_SPACE_TAG = "SharedSpace";
+    private static final String WORKSPACE_TAG = "WorkSpace";
+    private static final String USER_TAG = "User";
 
     private final ConnectionSettings connectionSettings = new ConnectionSettings();
 
     @Nullable
     @Override
-    public ConnectionSettings getState() {
-        return connectionSettings;
+    public Element getState() {
+        final Element element = new Element(CONNECTION_SETTINGS_TAG);
+        element.setAttribute(URL_TAG, String.valueOf(connectionSettings.getBaseUrl()));
+        element.setAttribute(SHARED_SPACE_TAG, String.valueOf(connectionSettings.getSharedSpaceId()));
+        element.setAttribute(WORKSPACE_TAG, String.valueOf(connectionSettings.getWorkspaceId()));
+        element.setAttribute(USER_TAG, String.valueOf(connectionSettings.getUserName()));
+
+        //save the password into the password store
+        encryptPassword(connectionSettings.getPassword());
+
+        return element;
     }
 
     @Override
-    public void loadState(ConnectionSettings state) {
-        setConnectionSettings(state);
+    public void loadState(Element state) {
+        connectionSettings.setBaseUrl(state.getAttributeValue(URL_TAG));
+        connectionSettings.setSharedSpaceId(Long.valueOf(state.getAttributeValue(SHARED_SPACE_TAG)));
+        connectionSettings.setWorkspaceId(Long.valueOf(state.getAttributeValue(WORKSPACE_TAG)));
+        connectionSettings.setUserName(state.getAttributeValue(USER_TAG));
+
+        //attempt to load password from the password store
+        connectionSettings.setPassword(decryptPassword());
+    }
+
+    @NotNull
+    private void encryptPassword(String password) {
+        try {
+            PasswordSafe.getInstance().storePassword (null, IdePersistentConnectionSettingsProvider.class, OCTANE_PASSWORD_KEY, password);
+        } catch (PasswordSafeException e) {
+            //log.info("Couldn't get password for key [" + OCTANE_PASSWORD_KEY + "]", e);
+        }
+    }
+
+    @NotNull
+    private String decryptPassword() {
+        String password;
+        try {
+            password = PasswordSafe.getInstance().getPassword(null, IdePersistentConnectionSettingsProvider.class, OCTANE_PASSWORD_KEY);
+        } catch (PasswordSafeException e) {
+            //log.info("Couldn't get password for key [" + OCTANE_PASSWORD_KEY + "]", e);
+            password = "";
+        }
+        return StringUtil.notNullize(password);
     }
 
     @Override
