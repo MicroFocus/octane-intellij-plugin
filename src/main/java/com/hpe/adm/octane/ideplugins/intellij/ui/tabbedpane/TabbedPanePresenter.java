d83df0850784dcd7cfd7173da74e136222ad5855
Tudor Dulau <dulau@hpe.com>
2016-11-25 14:44:50 +0200

    task #57003: Investigate how to download the gherkin test script from the plugin  - added code for downloading the script of a gherkin test into the IntelliJ project

--- src/main/java/com/hpe/adm/octane/ideplugins/intellij/ui/tabbedpane/TabbedPanePresenter.java
+++ src/main/java/com/hpe/adm/octane/ideplugins/intellij/ui/tabbedpane/TabbedPanePresenter.java
@@ -6,15 +6,33 @@ import com.hpe.adm.nga.sdk.model.EntityModel;
 import com.hpe.adm.octane.ideplugins.intellij.ui.Presenter;
 import com.hpe.adm.octane.ideplugins.intellij.ui.detail.EntityDetailPresenter;
 import com.hpe.adm.octane.ideplugins.intellij.ui.treetable.EntityTreeTablePresenter;
+import com.hpe.adm.octane.ideplugins.services.DownloadScriptService;
 import com.hpe.adm.octane.ideplugins.services.filtering.Filter;
+import com.intellij.ide.DataManager;
+import com.intellij.ide.actions.OpenProjectFileChooserDescriptor;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.actionSystem.DataKeys;
+import com.intellij.openapi.fileChooser.FileChooser;
+import com.intellij.openapi.fileChooser.FileChooserDescriptor;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
 
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TabbedPanePresenter implements Presenter<TabbedPaneView>{
+public class TabbedPanePresenter implements Presenter<TabbedPaneView> {
 
     @Inject
     TabbedPaneView tabbedPaneView;
+
+    @Inject
+    DownloadScriptService scriptService;
 
     @Inject
     Provider<EntityDetailPresenter> entityDetailPresenterProvider;
@@ -24,27 +42,27 @@ public class TabbedPanePresenter implements Presenter<TabbedPaneView>{
     List<EntityDetailPresenter> detailPresenters = new ArrayList<>();
     List<EntityTreeTablePresenter> treeTablePresenters = new ArrayList<>();
 
-    public void openMyWorkTab(String tabName){
+    public void openMyWorkTab(String tabName) {
         EntityTreeTablePresenter presenter = entityTreeTablePresenterProvider.get();
         treeTablePresenters.add(presenter);
         tabbedPaneView.addTab(tabName, presenter.getView().getComponent());
     }
 
-    public void openDetailTab(EntityModel entityModel){
+    public void openDetailTab(EntityModel entityModel) {
         EntityDetailPresenter presenter = entityDetailPresenterProvider.get();
         detailPresenters.add(presenter);
         tabbedPaneView.addTab("ETITTY NAME", presenter.getView().getComponent());
     }
 
-    public void openSearchTab(String searchQuery){
+    public void openSearchTab(String searchQuery) {
         //TODO: impl me
     }
 
-    public void openFilteredTab(String tabName, List<Filter> filters){
+    public void openFilteredTab(String tabName, List<Filter> filters) {
         //TODO: impl me
     }
 
-    public TabbedPaneView getView(){
+    public TabbedPaneView getView() {
         return tabbedPaneView;
     }
 
@@ -56,6 +74,57 @@ public class TabbedPanePresenter implements Presenter<TabbedPaneView>{
         //open test tabs
         openMyWorkTab("My work");
         openDetailTab(null);
+        openTestTab(1003);
     }
 
+    private VirtualFile chooseScriptFolder(Project project) {
+        FileChooserDescriptor descriptor = new OpenProjectFileChooserDescriptor(true);
+        descriptor.setHideIgnored(false);
+        descriptor.setRoots(project.getBaseDir());
+        descriptor.setTitle("Select Parent Folder");
+        descriptor.withTreeRootVisible(false);
+        VirtualFile[] virtualFile = FileChooser.chooseFiles(descriptor, null, null);
+
+        return (virtualFile.length != 1) ? null : virtualFile[0];
+    }
+
+    private File createTestScriptFile(String path, String fileName, String script) {
+        File f = new File(path + "/" + fileName);
+        try {
+            f.createNewFile();
+
+            if (script != null) {
+                Writer out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(f), StandardCharsets.UTF_8));
+                out.append(script);
+                out.flush();
+                out.close();
+            }
+        } catch (IOException e) {
+            return null;
+        }
+        return f;
+    }
+
+    private void openTestTab(int testId) {
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JButton button = new JButton("Test get script");
+        panel.add(button);
+        button.addActionListener((event) -> {
+            String scriptContent = scriptService.getGherkinTestScriptContent(testId);
+            if (scriptContent != null) {
+                DataContext dataContext = DataManager.getInstance().getDataContext();
+                Project project = DataKeys.PROJECT.getData(dataContext);
+                VirtualFile selectedFolder = chooseScriptFolder(project);
+                if (selectedFolder != null) {
+                    String scriptFileName = "test #" + testId + " script";
+                    File scriptFile = createTestScriptFile(selectedFolder.getPath(), scriptFileName, scriptContent);
+
+                    VirtualFile vFile = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(scriptFile);
+                    FileEditorManager.getInstance(project).openFile(vFile, true, true);
+                    project.getBaseDir().refresh(false, true);
+                }
+            }
+        });
+        tabbedPaneView.addTab("TEST TAB", panel);
+    }
 }
