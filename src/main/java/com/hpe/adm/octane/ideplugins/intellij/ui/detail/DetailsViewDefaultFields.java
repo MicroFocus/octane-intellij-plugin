d39229c44e06b00d09d5bfc80d679cd50a8ae2d7
Andras Toth <atoth@hpe.com>
2016-12-07 16:15:25 +0200

    Merge remote-tracking branch 'remotes/origin/custom_158003_My_Work'
    # Conflicts:
    #	src/main/java/com/hpe/adm/octane/ideplugins/intellij/ui/detail/DetailsViewDefaultFields.java

--- src/main/java/com/hpe/adm/octane/ideplugins/intellij/ui/detail/DetailsViewDefaultFields.java
+++ src/main/java/com/hpe/adm/octane/ideplugins/intellij/ui/detail/DetailsViewDefaultFields.java
@@ -19,7 +19,7 @@ public class DetailsViewDefaultFields {
     public static final String FIELD_SEVERITY = "severity";
     public static final String FIELD_GROUP = "group_udf";
     public static final String FIELD_ENVIROMENT = "taxonomies";
-    public static final String FIELD_FEEDBACKTYPE = "feedback_type_udf";
+    public static final String FIELD_FEEDBACKTYPE_UDF = "feedback_type_udf";
     public static final String FIELD_STORYPOINTS = "story_points";
     public static final String FIELD_DETECTEDINPPUSH = "detected_in_build";
     public static final String FIELD_DETECTEDINRELEASE = "detected_in_release";
