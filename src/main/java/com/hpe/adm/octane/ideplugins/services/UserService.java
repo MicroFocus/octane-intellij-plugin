bc02fcb25bd304bdf7fbf8c20a517f90aa7dc2e9
Andras Toth <atoth@hpe.com>
2017-01-31 17:15:03 +0200

    Current user from User service is now caches the current user until connections settings change

--- src/main/java/com/hpe/adm/octane/ideplugins/services/UserService.java
+++ src/main/java/com/hpe/adm/octane/ideplugins/services/UserService.java
@@ -5,6 +5,7 @@ import com.hpe.adm.nga.sdk.EntityList;
 import com.hpe.adm.nga.sdk.Octane;
 import com.hpe.adm.nga.sdk.Query;
 import com.hpe.adm.nga.sdk.model.EntityModel;
+import com.hpe.adm.octane.ideplugins.services.connection.ConnectionSettings;
 import com.hpe.adm.octane.ideplugins.services.connection.ConnectionSettingsProvider;
 import com.hpe.adm.octane.ideplugins.services.connection.OctaneProvider;
 import com.hpe.adm.octane.ideplugins.services.exception.ServiceRuntimeException;
@@ -20,6 +21,30 @@ public class UserService {
     @Inject
     private ConnectionSettingsProvider connectionSettingsProvider;
 
+    private EntityModel currentUserEntityModel;
+    private ConnectionSettings lastConnectionSettings;
+
+    private Runnable getCurrentUserRunnable = new Runnable() {
+        @Override
+        public void run() {
+            Octane octane = octaneProvider.getOctane();
+            String currentUser = connectionSettingsProvider.getConnectionSettings().getUserName();
+
+            EntityList entityList = octane.entityList(Entity.WORKSPACE_USER.getApiEntityName());
+            Collection<EntityModel> entityModels =
+                    entityList.get().query(
+                            new Query.QueryBuilder("name", Query::equalTo, currentUser).build())
+                            .execute();
+
+            if(entityModels.size()!=1){
+                throw new ServiceRuntimeException("Failed to retrieve logged in user id");
+            } else {
+                currentUserEntityModel = entityModels.iterator().next();
+            }
+        }
+    };
+
+
     /**
      * Well this is horrible, this method is needed because cross filtering work item owner by name does not work
      * @return
@@ -30,22 +55,14 @@ public class UserService {
     }
 
     public EntityModel getCurrentUser(){
-
-        Octane octane = octaneProvider.getOctane();
-        String currentUser = connectionSettingsProvider.getConnectionSettings().getUserName();
-
-        EntityList entityList = octane.entityList(Entity.WORKSPACE_USER.getApiEntityName());
-        Collection<EntityModel> entityModels =
-                entityList.get().query(
-                        new Query.QueryBuilder("name", Query::equalTo, currentUser).build())
-                        .execute();
-
-        if(entityModels.size()!=1){
-            throw new ServiceRuntimeException("Failed to retrieve logged in user id");
-        } else {
-            return entityModels.iterator().next();
+        if(currentUserEntityModel == null){
+            getCurrentUserRunnable.run();
+            lastConnectionSettings = connectionSettingsProvider.getConnectionSettings();
         }
-
+        else if(!lastConnectionSettings.equals(connectionSettingsProvider.getConnectionSettings())){
+            getCurrentUserRunnable.run();
+        }
+        return currentUserEntityModel;
     }
 
 }
