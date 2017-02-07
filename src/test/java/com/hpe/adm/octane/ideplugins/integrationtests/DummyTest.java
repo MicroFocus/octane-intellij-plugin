de4abef1f2a063af424449f48457f22a1563d1af
Andras Toth <atoth@hpe.com>
2017-02-07 18:29:35 +0200

    Added dummy dev unit test

---
+++ src/test/java/com/hpe/adm/octane/ideplugins/integrationtests/DummyTest.java
@@ -0,0 +1,103 @@
+package com.hpe.adm.octane.ideplugins.integrationtests;
+
+import com.google.inject.Inject;
+import com.hpe.adm.nga.sdk.Octane;
+import com.hpe.adm.nga.sdk.exception.OctaneException;
+import com.hpe.adm.nga.sdk.model.EntityModel;
+import com.hpe.adm.nga.sdk.model.MultiReferenceFieldModel;
+import com.hpe.adm.octane.ideplugins.services.CommentService;
+import com.hpe.adm.octane.ideplugins.services.EntityService;
+import com.hpe.adm.octane.ideplugins.services.UserService;
+import com.hpe.adm.octane.ideplugins.services.connection.OctaneProvider;
+import com.hpe.adm.octane.ideplugins.services.exception.ServiceException;
+import com.hpe.adm.octane.ideplugins.services.filtering.Entity;
+import hidden.edu.emory.mathcs.backport.java.util.Arrays;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class DummyTest {
+
+    @Inject
+    private EntityService entityService;
+
+    @Inject
+    private UserService userService;
+
+    @Inject
+    private OctaneProvider octaneProvider;
+
+    @Test
+    public void testytestest() {
+
+        EntityModel userStory = null;
+        try {
+            userStory = entityService.findEntity(Entity.USER_STORY, 1002L);
+        } catch (ServiceException e) {
+            e.printStackTrace();
+            System.out.println("tough luck");
+        }
+        EntityModel currentUser = userService.getCurrentUser();
+
+        MultiReferenceFieldModel referenceFieldModel1 = new MultiReferenceFieldModel("my_follow_items_owner", getSet(currentUser));
+        MultiReferenceFieldModel referenceFieldModel2 = new MultiReferenceFieldModel("my_new_items_owner", getSet(currentUser));
+
+        userStory.setValue(referenceFieldModel1);
+        userStory.setValue(referenceFieldModel2);
+
+        Octane octane = octaneProvider.getOctane();
+
+        Integer id = Integer.valueOf(userStory.getValue("id").getValue().toString());
+        EntityModel updateEntity = new EntityModel();
+        updateEntity.setValue(referenceFieldModel1);
+        updateEntity.setValue(referenceFieldModel2);
+
+        try {
+            octane.entityList(Entity.USER_STORY.getApiEntityName()).at(id).update().entity(updateEntity).execute();
+        } catch (OctaneException ex){
+            System.out.println(ex);
+        }
+
+    }
+
+    @Test
+    public void testytestest2() {
+
+        EntityModel userStory = null;
+        try {
+            userStory = entityService.findEntity(Entity.USER_STORY, 1002L);
+        } catch (ServiceException e) {
+            e.printStackTrace();
+            System.out.println("tough luck");
+        }
+        EntityModel currentUser = userService.getCurrentUser();
+
+        MultiReferenceFieldModel referenceFieldModel1 = new MultiReferenceFieldModel("my_follow_items_owner", java.util.Collections.emptySet());
+        MultiReferenceFieldModel referenceFieldModel2 = new MultiReferenceFieldModel("my_new_items_owner", java.util.Collections.emptySet());
+
+        userStory.setValue(referenceFieldModel1);
+        userStory.setValue(referenceFieldModel2);
+
+        Octane octane = octaneProvider.getOctane();
+
+        Integer id = Integer.valueOf(userStory.getValue("id").getValue().toString());
+        EntityModel updateEntity = new EntityModel();
+        updateEntity.setValue(referenceFieldModel1);
+        updateEntity.setValue(referenceFieldModel2);
+
+        try {
+            octane.entityList(Entity.USER_STORY.getApiEntityName()).at(id).update().entity(updateEntity).execute();
+        } catch (OctaneException ex){
+            System.out.println(ex);
+        }
+
+    }
+
+
+    private Set getSet(Object... fm) {
+        Set set = new HashSet<>(Arrays.asList(fm));
+        return set;
+    }
+
+}
