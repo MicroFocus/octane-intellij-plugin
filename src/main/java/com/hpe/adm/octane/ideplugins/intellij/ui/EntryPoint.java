7418052ee498009c8806c92c1571682cc72e61ce
Andras Toth <atoth@hpe.com>
2017-02-13 11:46:15 +0200

    Added eager init for meta service

--- src/main/java/com/hpe/adm/octane/ideplugins/intellij/ui/EntryPoint.java
+++ src/main/java/com/hpe/adm/octane/ideplugins/intellij/ui/EntryPoint.java
@@ -6,10 +6,12 @@ import com.hpe.adm.octane.ideplugins.intellij.ui.components.WelcomeViewComponent
 import com.hpe.adm.octane.ideplugins.intellij.ui.customcomponents.LoadingWidget;
 import com.hpe.adm.octane.ideplugins.intellij.ui.main.MainPresenter;
 import com.hpe.adm.octane.ideplugins.intellij.ui.util.UiUtil;
+import com.hpe.adm.octane.ideplugins.services.MetadataService;
 import com.hpe.adm.octane.ideplugins.services.TestService;
 import com.hpe.adm.octane.ideplugins.services.connection.ConnectionSettings;
 import com.hpe.adm.octane.ideplugins.services.connection.ConnectionSettingsProvider;
 import com.hpe.adm.octane.ideplugins.services.exception.ServiceException;
+import com.hpe.adm.octane.ideplugins.services.filtering.Entity;
 import com.hpe.adm.octane.ideplugins.services.nonentity.SharedSpaceLevelRequestService;
 import com.intellij.notification.NotificationType;
 import com.intellij.openapi.diagnostic.Logger;
@@ -34,6 +36,7 @@ public class EntryPoint implements ToolWindowFactory {
      * This method can be called by multiple IntelliJ's running at the same time,
      * it's important that the state of any one tool window that contains the IDE plugin
      * does not affect the state of other potentially open tool windows
+     *
      * @param project
      * @param toolWindow
      */
@@ -51,62 +54,64 @@ public class EntryPoint implements ToolWindowFactory {
 
         Runnable mainToolWindowContentControl = () -> {
 
-            EntryPoint.this.setContent(toolWindow, () -> new LoadingWidget(), "Loading");
+            EntryPoint.this.setContent(toolWindow, () -> new LoadingWidget(), "Loading Workspace");
 
-            Task.Backgroundable backgroundTask = new Task.Backgroundable(project, "Loading workspace", false) {
+            Task.Backgroundable backgroundTask = new Task.Backgroundable(project, "Loading Workspace", false) {
                 public void run(@NotNull ProgressIndicator indicator) {
-                    //SwingUtilities.invokeLater(() -> {
-                        try {
-                            ConnectionSettings connectionSettings = connectionSettingsProvider.getConnectionSettings();
+                    try {
+                        ConnectionSettings connectionSettings = connectionSettingsProvider.getConnectionSettings();
 
-                            if (connectionSettings == null || connectionSettings.isEmpty()) {
-                                throw new ServiceException("No connection settings configured");
-                            }
-
-                            TestService testService = pluginModule.getInstance(TestService.class);
-                            testService.testConnection(connectionSettings);
-
-                            // Make sure you only instantiate other services (including the ones in the Presenter hierarchy,
-                            // after you tested the connection settings with the test service
-
-                            //Add the workspace name to the ToolWindow content tab name
-                            SharedSpaceLevelRequestService sharedSpaceLevelRequestService = pluginModule.getInstance(SharedSpaceLevelRequestService.class);
-                            String workspaceDisplayName = " [" + sharedSpaceLevelRequestService.getCurrentWorkspaceName() + "]";
-
-                            SwingUtilities.invokeLater(() -> {
-                                //Create the presenter hierarchy, DI will inject view instances
-                                MainPresenter mainPresenter = pluginModule.getInstance(MainPresenter.class);
-                                setContent(toolWindow, mainPresenter.getView(), workspaceDisplayName);
-                            });
-
-                        } catch (Exception ex) {
-                            pluginModule.getInstance(IdePluginPersistentState.class).clearState(IdePluginPersistentState.Key.ACTIVE_WORK_ITEM);
-                            WelcomeViewComponent welcomeViewComponent;
-
-                            // If there were previously configured connection settings
-                            // show a slightly different message in the welcome view
-                            if (!connectionSettingsProvider.getConnectionSettings().isEmpty()) {
-                                welcomeViewComponent = new WelcomeViewComponent(
-                                        project,
-                                        "Your previously saved connection settings do not seem to work",
-                                        "Please go to settings and test your connection to Octane");
-
-                                //also show a notification with the exception
-                                UiUtil.showWarningBalloon(project,
-                                        "Failed to connect to Octane",
-                                        "Your previously saved connection settings do not seem to work <br> Error: " + ex.getMessage(),
-                                        NotificationType.WARNING);
-                            } else {
-                                //In this case (probably), the plugin was never configured on this project before
-                                welcomeViewComponent = new WelcomeViewComponent(project);
-                            }
-
-                            log.info("Showing welcome view, cause: " + ex);
-
-                            //Show the welcome view
-                            setContent(toolWindow, welcomeViewComponent, "");
+                        if (connectionSettings == null || connectionSettings.isEmpty()) {
+                            throw new ServiceException("No connection settings configured");
                         }
-                    //});
+
+                        TestService testService = pluginModule.getInstance(TestService.class);
+                        testService.testConnection(connectionSettings);
+
+                        // Make sure you only instantiate other services (including the ones in the Presenter hierarchy,
+                        // after you tested the connection settings with the test service
+
+                        //Add the workspace name to the ToolWindow content tab name
+                        SharedSpaceLevelRequestService sharedSpaceLevelRequestService = pluginModule.getInstance(SharedSpaceLevelRequestService.class);
+                        String workspaceDisplayName = " [" + sharedSpaceLevelRequestService.getCurrentWorkspaceName() + "]";
+
+                        //Eager init the metadata service, it's an eager singleton
+                        MetadataService metadataService = pluginModule.getInstance(MetadataService.class);
+                        metadataService.eagerInit(Entity.values());
+
+                        SwingUtilities.invokeLater(() -> {
+                            //Create the presenter hierarchy, DI will inject view instances
+                            MainPresenter mainPresenter = pluginModule.getInstance(MainPresenter.class);
+                            setContent(toolWindow, mainPresenter.getView(), workspaceDisplayName);
+                        });
+
+                    } catch (Exception ex) {
+                        pluginModule.getInstance(IdePluginPersistentState.class).clearState(IdePluginPersistentState.Key.ACTIVE_WORK_ITEM);
+                        WelcomeViewComponent welcomeViewComponent;
+
+                        // If there were previously configured connection settings
+                        // show a slightly different message in the welcome view
+                        if (!connectionSettingsProvider.getConnectionSettings().isEmpty()) {
+                            welcomeViewComponent = new WelcomeViewComponent(
+                                    project,
+                                    "Your previously saved connection settings do not seem to work",
+                                    "Please go to settings and test your connection to Octane");
+
+                            //also show a notification with the exception
+                            UiUtil.showWarningBalloon(project,
+                                    "Failed to connect to Octane",
+                                    "Your previously saved connection settings do not seem to work <br> Error: " + ex.getMessage(),
+                                    NotificationType.WARNING);
+                        } else {
+                            //In this case (probably), the plugin was never configured on this project before
+                            welcomeViewComponent = new WelcomeViewComponent(project);
+                        }
+
+                        log.info("Showing welcome view, cause: " + ex);
+
+                        //Show the welcome view
+                        setContent(toolWindow, welcomeViewComponent, "");
+                    }
                 }
             };
             backgroundTask.queue();
