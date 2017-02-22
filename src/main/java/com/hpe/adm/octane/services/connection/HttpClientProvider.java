d41b33a76d43ecfc79dc624b1c171768f8bc5f5e
Alexandra Marinescu <alexandra.marinescu@hpe.com>
2017-02-22 14:31:50 +0200

    removed services from the plugin as they will be used from a common component

--- src/main/java/com/hpe/adm/octane/services/connection/HttpClientProvider.java
+++
@@ -1,8 +0,0 @@
-package com.hpe.adm.octane.services.connection;
-
-import com.hpe.adm.nga.sdk.network.OctaneHttpClient;
-
-
-public interface HttpClientProvider {
-    OctaneHttpClient geOctaneHttpClient();
-}
