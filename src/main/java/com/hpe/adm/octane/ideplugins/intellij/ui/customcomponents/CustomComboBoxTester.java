7c06f331cd6a2ee46f7d6ae37efeb55e27d7ffe8
vladbenchea <vlad.benchea@hpe.com>
2018-01-03 11:11:27 +0200

    Revert "Updated copyrights from 2017 to 2018"
    This reverts commit 6aaf142

--- src/main/java/com/hpe/adm/octane/ideplugins/intellij/ui/customcomponents/CustomComboBoxTester.java
+++ src/main/java/com/hpe/adm/octane/ideplugins/intellij/ui/customcomponents/CustomComboBoxTester.java
@@ -1,5 +1,5 @@
 /*
- * © 2018 EntIT Software LLC, a Micro Focus company, L.P.
+ * © 2017 EntIT Software LLC, a Micro Focus company, L.P.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
--- src/main/java/com/hpe/adm/octane/ideplugins/intellij/ui/detail/entity/RequirementsDetailsPanel.java
+++ src/main/java/com/hpe/adm/octane/ideplugins/intellij/ui/detail/entity/RequirementsDetailsPanel.java
@@ -1,7 +1,7 @@
 package com.hpe.adm.octane.ideplugins.intellij.ui.detail.entity;
 
 /*
- * © 2018 EntIT Software LLC, a Micro Focus company, L.P.
+ * © 2017 EntIT Software LLC, a Micro Focus company, L.P.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
