33cc36128e7e7388be6e2e3acec869dfc6497550
Kadar Tibor Manasse <kadar-tibor.manasse@hpe.com>
2018-04-26 16:34:43 +0300

    implements method to convert description

--- src/main/java/com/hpe/adm/octane/ideplugins/intellij/ui/detail/EntityDetailPresenter.java
+++ src/main/java/com/hpe/adm/octane/ideplugins/intellij/ui/detail/EntityDetailPresenter.java
@@ -28,6 +28,7 @@ import com.hpe.adm.octane.ideplugins.intellij.util.RestUtil;
 import com.hpe.adm.octane.ideplugins.services.CommentService;
 import com.hpe.adm.octane.ideplugins.services.EntityService;
 import com.hpe.adm.octane.ideplugins.services.MetadataService;
+import com.hpe.adm.octane.ideplugins.services.connection.ClientLoginCookie;
 import com.hpe.adm.octane.ideplugins.services.exception.ServiceException;
 import com.hpe.adm.octane.ideplugins.services.filtering.Entity;
 import com.hpe.adm.octane.ideplugins.services.nonentity.ImageService;
@@ -39,8 +40,10 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.IconLoader;
 import com.intellij.openapi.vcs.VcsShowConfirmationOption;
 import com.intellij.util.ui.ConfirmationDialog;
+import javafx.application.Platform;
 import org.jetbrains.annotations.Nullable;
 
+import javax.swing.*;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
@@ -60,6 +63,8 @@ public class EntityDetailPresenter implements Presenter<EntityDetailView> {
     private MetadataService metadataService;
     @Inject
     private ImageService imageService;
+    @Inject
+    private ClientLoginCookie clientLoginCookie;
 
     private EntityDetailView entityDetailView;
     private Entity entityType;
@@ -86,6 +91,56 @@ public class EntityDetailPresenter implements Presenter<EntityDetailView> {
         this.entityType = entityType;
         this.entityId = entityId;
 
+        Thread simpleThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    fields = metadataService.getVisibleFields(entityType);
+
+                    Set<String> requestedFields = fields.stream().map(FieldMetadata::getName).collect(Collectors.toSet());
+                    entityModel = entityService.findEntity(entityType, entityId, requestedFields);
+
+                    // /change relative urls with local paths to temp directory and download images
+                    String description = Util.getUiDataFromModel(entityModel.getValue(DetailsViewDefaultFields.FIELD_DESCRIPTION));
+                    description = HtmlTextEditor.removeHtmlStructure(description);
+                    description = imageService.downloadPictures(description);
+                    entityModel.setValue(new StringFieldModel("description", description));
+
+                    //The subtype field is absolutely necessary, yet the server sometimes has weird ideas, and doesn't return it
+                    if (entityType.isSubtype()) {
+                        entityModel.setValue(new StringFieldModel(DetailsViewDefaultFields.FIELD_SUBTYPE, entityType.getSubtypeName()));
+                    }
+                } catch (ServiceException ex) {
+                    entityDetailView.setErrorMessage(ex.getMessage());
+                }
+
+                SwingUtilities.invokeLater(() -> {
+                    if (entityModel != null) {
+                        entityDetailView.createDetailsPanel(entityModel, fields);
+                        entityDetailView.setSaveSelectedPhaseButton(new SaveSelectedPhaseAction());
+                        entityDetailView.setRefreshEntityButton(new EntityRefreshAction());
+                        if (entityType != TASK) {
+                            entityDetailView.setCommentsEntityButton(new EntityCommentsAction());
+                            setComments(entityModel);
+                            addSendNewCommentAction(entityModel);
+                        }
+                        if (entityType != MANUAL_TEST_RUN && entityType != TEST_SUITE_RUN) {
+                            setPossibleTransitions(entityModel);
+                            entityDetailView.setPhaseInHeader(true);
+                        } else {
+                            entityDetailView.removeSaveSelectedPhaseButton();
+                            entityDetailView.setPhaseInHeader(false);
+                        }
+                        entityDetailView.setFieldSelectButton(new SelectFieldsAction());
+                        //Title goes to browser
+                        entityDetailView.setEntityNameClickHandler(() -> entityService.openInBrowser(entityModel));
+                    }
+                });
+            }
+        });
+        simpleThread.start();
+
+        /*
         RestUtil.runInBackground(
                 () -> {
                     try {
@@ -93,12 +148,6 @@ public class EntityDetailPresenter implements Presenter<EntityDetailView> {
 
                         Set<String> requestedFields = fields.stream().map(FieldMetadata::getName).collect(Collectors.toSet());
                         entityModel = entityService.findEntity(this.entityType, this.entityId, requestedFields);
-
-                        //change relative urls with local paths to temp and download images
-                        String description = Util.getUiDataFromModel(entityModel.getValue(DetailsViewDefaultFields.FIELD_DESCRIPTION));
-                        description = HtmlTextEditor.removeHtmlStructure(description);
-                        description = imageService.downloadPictures(description);
-                        entityModel.setValue(new StringFieldModel("description", description));
 
                         //The subtype field is absolutely necessary, yet the server sometimes has weird ideas, and doesn't return it
                         if (entityType.isSubtype()) {
@@ -115,6 +164,11 @@ public class EntityDetailPresenter implements Presenter<EntityDetailView> {
                     if (entityModel != null) {
                         this.entityModel = entityModel;
 
+                        // /change relative urls with local paths to temp and download images
+                        String description = Util.getUiDataFromModel(entityModel.getValue(DetailsViewDefaultFields.FIELD_DESCRIPTION));
+                        description = HtmlTextEditor.removeHtmlStructure(description);
+                        description = imageService.downloadPictures(description);
+                        entityModel.setValue(new StringFieldModel("description", description));
 
                         entityDetailView.createDetailsPanel(entityModel, fields);
                         entityDetailView.setSaveSelectedPhaseButton(new SaveSelectedPhaseAction());
@@ -139,8 +193,7 @@ public class EntityDetailPresenter implements Presenter<EntityDetailView> {
                 null,
                 null,
                 "Loading entity " + entityType.name() + ": " + entityId);
-
-
+               */
     }
 
     private void setPossibleTransitions(EntityModel entityModel) {
