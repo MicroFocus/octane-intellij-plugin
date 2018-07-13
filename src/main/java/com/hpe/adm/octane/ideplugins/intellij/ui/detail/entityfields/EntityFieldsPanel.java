e3bd501352a88b13e07176662b9809a27a42b897
Kadar Tibor Manasse <kadar-tibor.manasse@hpe.com>
2018-07-13 11:25:23 +0300

    fixes not aligned fields in detail view

--- src/main/java/com/hpe/adm/octane/ideplugins/intellij/ui/detail/entityfields/EntityFieldsPanel.java
+++ src/main/java/com/hpe/adm/octane/ideplugins/intellij/ui/detail/entityfields/EntityFieldsPanel.java
@@ -40,17 +40,17 @@ public class EntityFieldsPanel extends JXPanel {
 
     public EntityFieldsPanel() {
         GridBagLayout gbl = new GridBagLayout();
-        gbl.columnWidths = new int[]{0,0};
+        gbl.columnWidths = new int[]{0, 0};
         gbl.columnWeights = new double[]{0.0, 0.0};
-        gbl.rowHeights = new int[]{0,0};
-        gbl.rowWeights = new double[]{0.0,0.0};
+        gbl.rowHeights = new int[]{0, 0};
+        gbl.rowWeights = new double[]{0.0, 0.0};
         setLayout(gbl);
 
         generalLabel = new JXLabel("General");
         generalLabel.setFont(new Font(generalLabel.getFont().getName(), Font.BOLD, 18));
         GridBagConstraints gbc_GeneralTitle = new GridBagConstraints();
         gbc_GeneralTitle.anchor = GridBagConstraints.WEST;
-        gbc_GeneralTitle.insets = new Insets(5,0, 10, 0);
+        gbc_GeneralTitle.insets = new Insets(5, 0, 10, 0);
         gbc_GeneralTitle.gridx = 0;
         gbc_GeneralTitle.gridy = 0;
         add(generalLabel, gbc_GeneralTitle);
@@ -74,7 +74,7 @@ public class EntityFieldsPanel extends JXPanel {
         detailsRightPanel.setLayout(gbl_detailsRightPanel);
         GridBagConstraints gbc_rightPanel = new GridBagConstraints();
         gbc_rightPanel.fill = GridBagConstraints.HORIZONTAL;
-        gbc_rightPanel.anchor = GridBagConstraints.WEST;
+        gbc_rightPanel.anchor = GridBagConstraints.NORTH;
         gbc_rightPanel.gridx = 1;
         gbc_rightPanel.gridy = 1;
         gbc_rightPanel.weightx = 1.0;
@@ -106,20 +106,20 @@ public class EntityFieldsPanel extends JXPanel {
     public void setEntityModel(EntityModelWrapper entityModelWrapper, Set<String> fieldNames) {
         detailsLeftPanel.removeAll();
         detailsRightPanel.removeAll();
-
+        JXLabel fieldLabel = null;
         int fieldCount = 0;
         int i = 0;
         for (FieldMetadata fieldMetadata : fields) {
             if (fieldNames.contains(fieldMetadata.getName())) {
                 String fieldName = fieldMetadata.getName();
-                JXLabel fieldLabel = new JXLabel();
+                fieldLabel = new JXLabel();
                 Font font = new Font(fieldLabel.getFont().getFontName(), Font.BOLD, fieldLabel.getFont().getSize());
                 fieldLabel.setFont(font);
                 fieldLabel.setText(fieldMetadata.getLabel());
                 GridBagConstraints gbc1 = new GridBagConstraints();
                 gbc1.anchor = GridBagConstraints.WEST;
                 gbc1.fill = GridBagConstraints.HORIZONTAL;
-                gbc1.insets = new Insets(0,0,5, 0);
+                gbc1.insets = new Insets(0, 0, 5, 0);
                 gbc1.gridx = 0;
                 gbc1.gridy = i;
 
@@ -144,17 +144,6 @@ public class EntityFieldsPanel extends JXPanel {
             }
         }
 
-        if (fieldCount % 2 == 1) {
-            JXLabel emptyLabel = new JXLabel();
-            GridBagConstraints gbc = new GridBagConstraints();
-            gbc.insets = new Insets(0, 10, 5, 5);
-            gbc.anchor = GridBagConstraints.CENTER;
-            gbc.fill = GridBagConstraints.BOTH;
-            gbc.gridx = 0;
-            gbc.gridy = i;
-            gbc.gridwidth = 2;
-            detailsRightPanel.add(emptyLabel, gbc);
-        }
         detailsLeftPanel.repaint();
         detailsLeftPanel.revalidate();
         detailsRightPanel.repaint();
