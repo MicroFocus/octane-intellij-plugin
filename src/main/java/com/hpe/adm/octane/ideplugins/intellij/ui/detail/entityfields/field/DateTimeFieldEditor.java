515c473361fdde02f8b85d1295b8e1c3f3c5934f
kdrtibor <kadar-tibor.manasse@hpe.com>
2018-07-24 15:05:22 +0300

    updates date and time picker to be more user friendly

--- src/main/java/com/hpe/adm/octane/ideplugins/intellij/ui/detail/entityfields/field/DateTimeFieldEditor.java
+++ src/main/java/com/hpe/adm/octane/ideplugins/intellij/ui/detail/entityfields/field/DateTimeFieldEditor.java
@@ -6,18 +6,22 @@ import com.hpe.adm.nga.sdk.model.ReferenceFieldModel;
 import com.hpe.adm.octane.ideplugins.intellij.ui.Constants;
 import com.hpe.adm.octane.ideplugins.services.model.EntityModelWrapper;
 import com.intellij.openapi.util.IconLoader;
+import com.michaelbaranov.microba.calendar.DatePicker;
 
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.swing.text.NumberFormatter;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.font.TextAttribute;
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.beans.PropertyVetoException;
+import java.time.*;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Map;
 
 public class DateTimeFieldEditor extends FieldEditor {
@@ -32,15 +36,11 @@ public class DateTimeFieldEditor extends FieldEditor {
     private JSpinner secondsSpinner;
     private JSpinner dayTimeSpinner;
 
-    private JLabel yearLabel;
-    private JSpinner yearSpinner;
-    private JLabel monthLabel;
-    private JSpinner monthSpinner;
-    private JLabel dayLabel;
-    private JSpinner daySpinner;
+    private DatePicker microbaDatePicker;
 
     private JLabel linkToButtons;
     private JLabel clearSelection;
+
 
     public DateTimeFieldEditor() {
 
@@ -49,35 +49,31 @@ public class DateTimeFieldEditor extends FieldEditor {
         gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
         setLayout(gridBagLayout);
 
-        yearLabel = new JLabel("y :");
-        monthLabel = new JLabel("m :");
-        dayLabel = new JLabel("d :");
+        microbaDatePicker = new DatePicker();
+        microbaDatePicker.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleChange();
+            }
+        });
 
-        SpinnerModel yearSpinnerModel = new SpinnerNumberModel(2018, 1950, 2100, 1);
-        yearSpinner = new JSpinner(yearSpinnerModel);
-        ((JSpinner.DefaultEditor) yearSpinner.getEditor()).getTextField().setEditable(false);
-
-        SpinnerModel monthSpinnerModel = new SpinnerNumberModel(1, 1, 12, 1);
-        monthSpinner = new JSpinner(monthSpinnerModel);
-        ((JSpinner.DefaultEditor) monthSpinner.getEditor()).getTextField().setEditable(false);
-
-        SpinnerModel daySpinnerModel = new SpinnerNumberModel(1, 1, 31, 1);
-        daySpinner = new JSpinner(daySpinnerModel);
-        ((JSpinner.DefaultEditor) daySpinner.getEditor()).getTextField().setEditable(false);
 
         timeLabel = new JLabel("time :");
 
         SpinnerModel hourSpinnerModel = new SpinnerNumberModel(0, 0, 11, 1);
         hourSpinner = new JSpinner(hourSpinnerModel);
-        ((JSpinner.DefaultEditor) hourSpinner.getEditor()).getTextField().setEditable(false);
+        JFormattedTextField txt = ((JSpinner.NumberEditor) hourSpinner.getEditor()).getTextField();
+        ((NumberFormatter) txt.getFormatter()).setAllowsInvalid(false);
 
         SpinnerModel minuteSpinnerModel = new SpinnerNumberModel(0, 0, 59, 1);
         minuteSpinner = new JSpinner(minuteSpinnerModel);
-        ((JSpinner.DefaultEditor) minuteSpinner.getEditor()).getTextField().setEditable(false);
+        JFormattedTextField txt1 = ((JSpinner.NumberEditor) minuteSpinner.getEditor()).getTextField();
+        ((NumberFormatter) txt1.getFormatter()).setAllowsInvalid(false);
 
         SpinnerModel secondsSpinnerModel = new SpinnerNumberModel(0, 0, 59, 1);
         secondsSpinner = new JSpinner(secondsSpinnerModel);
-        ((JSpinner.DefaultEditor) secondsSpinner.getEditor()).getTextField().setEditable(false);
+        JFormattedTextField txt2 = ((JSpinner.NumberEditor) minuteSpinner.getEditor()).getTextField();
+        ((NumberFormatter) txt2.getFormatter()).setAllowsInvalid(false);
 
         SpinnerModel daytimeSpinnerModel = new SpinnerListModel(Arrays.asList("AM", "PM"));
         dayTimeSpinner = new JSpinner(daytimeSpinnerModel);
@@ -105,7 +101,6 @@ public class DateTimeFieldEditor extends FieldEditor {
 
         clearSelection = new JLabel();
         clearSelection.setIcon(IconLoader.findIcon(Constants.IMG_REMOVE_SELECTION));
-//        btnSetNull.setImage(ImageResources.OCTANE_REMOVE.getImage());
 
         // Nullify
         clearSelection.addMouseListener(new MouseAdapter() {
@@ -124,28 +119,6 @@ public class DateTimeFieldEditor extends FieldEditor {
                 ZonedDateTime now = ZonedDateTime.now();
                 setZonedDateTime(now);
                 entityModelWrapper.setValue(new DateFieldModel(fieldName, now));
-            }
-        });
-
-        yearSpinner.addChangeListener(new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                handleChange();
-            }
-        });
-
-        monthSpinner.addChangeListener(new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                handleChange();
-                changeDaySpinnerModel();
-            }
-        });
-
-        daySpinner.addChangeListener(new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                handleChange();
             }
         });
 
@@ -179,42 +152,6 @@ public class DateTimeFieldEditor extends FieldEditor {
         entityModelWrapper.setValue(new DateFieldModel(fieldName, getZonedDateTime()));
     }
 
-    private boolean isleapYear(int year){
-        if(year % 4 != 0){
-            return false;
-        } else {
-            if(year % 100 != 0) {
-                return true;
-            } else {
-                if(year % 400 != 0){
-                    return false;
-                } else return true;
-            }
-        }
-
-    }
-
-    private void changeDaySpinnerModel() {
-        switch ((int) monthSpinner.getValue()) {
-            case 2:
-                if(isleapYear((int) yearSpinner.getValue())){
-                    daySpinner.setModel(new SpinnerNumberModel(1, 1, 29, 1));
-                } else {
-                    daySpinner.setModel(new SpinnerNumberModel(1, 1, 29, 1));
-                }
-                break;
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-                daySpinner.setModel(new SpinnerNumberModel(1, 1, 30, 1));
-                break;
-            default:
-                daySpinner.setModel(new SpinnerNumberModel(1, 1, 31, 1));
-        }
-        ((JSpinner.DefaultEditor) daySpinner.getEditor()).getTextField().setEditable(false);
-    }
-
     private void addElementToPosition(Component cmp, int x) {
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.anchor = GridBagConstraints.WEST;
@@ -226,27 +163,24 @@ public class DateTimeFieldEditor extends FieldEditor {
 
     private void setDateTimeVisible() {
         removeAll();
-        addElementToPosition(dayLabel, 0);
-        addElementToPosition(daySpinner, 1);
-        addElementToPosition(monthLabel, 2);
-        addElementToPosition(monthSpinner, 3);
-        addElementToPosition(yearLabel, 4);
-        addElementToPosition(yearSpinner, 5);
-        addElementToPosition(timeLabel, 6);
-        addElementToPosition(hourSpinner, 7);
-        addElementToPosition(minuteSpinner, 8);
-        addElementToPosition(secondsSpinner, 9);
-        addElementToPosition(dayTimeSpinner, 10);
+        addElementToPosition(microbaDatePicker, 0);
+        addElementToPosition(timeLabel, 1);
+        addElementToPosition(hourSpinner, 2);
+        addElementToPosition(new JLabel(":"), 3);
+        addElementToPosition(minuteSpinner, 4);
+        addElementToPosition(new JLabel(":"), 5);
+        addElementToPosition(secondsSpinner, 6);
+        addElementToPosition(dayTimeSpinner, 7);
 
         GridBagConstraints gbc_clearSelection = new GridBagConstraints();
         gbc_clearSelection.anchor = GridBagConstraints.CENTER;
         gbc_clearSelection.fill = GridBagConstraints.HORIZONTAL;
         gbc_clearSelection.insets = new Insets(0, 0, 0, 5);
-        gbc_clearSelection.gridx = 11;
+        gbc_clearSelection.gridx = 8;
         gbc_clearSelection.weightx = 1.0;
         add(new JLabel(), gbc_clearSelection);
 
-        addElementToPosition(clearSelection, 12);
+        addElementToPosition(clearSelection, 9);
         revalidate();
     }
 
@@ -269,9 +203,17 @@ public class DateTimeFieldEditor extends FieldEditor {
             Instant timeStamp = zonedDateTime.toInstant();
             zonedDateTime = timeStamp.atZone(ZoneId.systemDefault());
 
-            yearSpinner.setValue(zonedDateTime.getYear());
-            monthSpinner.setValue(zonedDateTime.getMonthValue());
-            daySpinner.setValue(zonedDateTime.getDayOfMonth());
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.YEAR, zonedDateTime.getYear());
+            //Date uses 0 based numbering while ZonedDateTime uses 1 based numbering
+            calendar.set(Calendar.MONTH, zonedDateTime.getMonthValue() - 1);
+            calendar.set(Calendar.DATE, zonedDateTime.getDayOfMonth());
+
+            try {
+                microbaDatePicker.setDate(calendar.getTime());
+            } catch (PropertyVetoException e) {
+            }
+
             if (zonedDateTime.getHour() >= 12) {
                 hourSpinner.setValue(zonedDateTime.getHour() - 12);
                 dayTimeSpinner.setValue("PM");
@@ -286,16 +228,17 @@ public class DateTimeFieldEditor extends FieldEditor {
     }
 
     private ZonedDateTime getZonedDateTime() {
-
+        LocalDate localDate = microbaDatePicker.getDate()
+                .toInstant()
+                .atZone(ZoneId.systemDefault()).toLocalDate();
+        //Date uses 0 based numbering while ZonedDateTime uses 1 based numbering
+        localDate.plusMonths(1);
         // Converting to UTC is not necessary, the SDK will do it for you
-        return ZonedDateTime.of(
-                (int) yearSpinner.getValue(),
-                (int) monthSpinner.getValue(),
-                (int) daySpinner.getValue(),
-                dayTimeSpinner.getValue().equals("AM") ? (int) hourSpinner.getValue() : (int) hourSpinner.getValue() + 12,
-                (int) minuteSpinner.getValue(),
-                (int) secondsSpinner.getValue(),
-                0,
+        return ZonedDateTime.of(localDate,
+                LocalTime.of(
+                        dayTimeSpinner.getValue().equals("AM") ? (int) hourSpinner.getValue() : (int) hourSpinner.getValue() + 12,
+                        (int) minuteSpinner.getValue(),
+                        (int) secondsSpinner.getValue()),
                 ZoneId.systemDefault());
 
     }
