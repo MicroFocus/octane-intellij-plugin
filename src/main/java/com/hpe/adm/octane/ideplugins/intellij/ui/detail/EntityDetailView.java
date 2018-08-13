ce52332c6f4603a77ed75e4ba2e8a990a40a298e
kdrtibor <kadar-tibor.manasse@hpe.com>
2018-08-13 10:04:45 +0300

    fixes description

--- src/main/java/com/hpe/adm/octane/ideplugins/intellij/ui/detail/EntityDetailView.java
+++ src/main/java/com/hpe/adm/octane/ideplugins/intellij/ui/detail/EntityDetailView.java
@@ -87,7 +87,7 @@ public class EntityDetailView extends JPanel implements View, Scrollable {
         gridBagLayout.columnWidths = new int[]{0, 0, 0};
         gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0};
         gridBagLayout.columnWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
-        gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+        gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
         setLayout(gridBagLayout);
 
         Color separatorColor = UIManager.getColor("Separator.foreground");
