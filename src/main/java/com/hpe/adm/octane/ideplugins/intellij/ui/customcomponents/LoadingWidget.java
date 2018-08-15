485ae1c76894a72303db3be47a0f9333d2e68408
kdrtibor <kadar-tibor.manasse@hpe.com>
2018-08-15 10:18:31 +0300

    removes listener from loading widget, hence makes it garbage collectible

--- src/main/java/com/hpe/adm/octane/ideplugins/intellij/ui/customcomponents/LoadingWidget.java
+++ src/main/java/com/hpe/adm/octane/ideplugins/intellij/ui/customcomponents/LoadingWidget.java
@@ -14,7 +14,6 @@
 package com.hpe.adm.octane.ideplugins.intellij.ui.customcomponents;
 
 import com.hpe.adm.octane.ideplugins.intellij.ui.Constants;
-import com.intellij.ui.JBColor;
 
 import javax.swing.*;
 import java.awt.*;
@@ -26,19 +25,11 @@ public class LoadingWidget extends JPanel {
     }
 
     public LoadingWidget(String loadingMessage) {
-        setLayout(new BorderLayout(0,0));
+        setLayout(new BorderLayout(0, 0));
         ImageIcon pacmanImage = new ImageIcon(LoadingWidget.class.getClassLoader().getResource(Constants.IMG_AJAX_SPINNER));
         JLabel loadingLabel = new JLabel(pacmanImage);
         //loadingLabel.setText(loadingMessage);
         add(loadingLabel, BorderLayout.CENTER);
-
-        //Make sure all saved instances have the correct background color after a LAF change
-        UIManager.addPropertyChangeListener(evt -> {
-            if("lookAndFeel".equals(evt.getPropertyName())){
-                setOpaque(true);
-                setBackground(JBColor.background());
-            }
-        });
     }
 
 }
