e492e1f2ab4ba8d40e7286a8e56042ce07e19c45
Andras Toth <atoth@microfocus.com>
2019-05-13 15:29:45 +0300

    removing debug code

--- src/main/java/com/hpe/adm/octane/ideplugins/intellij/util/JavaFxUtils.java
+++ src/main/java/com/hpe/adm/octane/ideplugins/intellij/util/JavaFxUtils.java
@@ -3,13 +3,12 @@ package com.hpe.adm.octane.ideplugins.intellij.util;
 public class JavaFxUtils {
 
     public static boolean isJavaFxAvailable() {
-        return false;
-//        try {
-//            JavaFxUtils.class.getClassLoader().loadClass("javafx.embed.swing.JFXPanel");
-//            return true;
-//        } catch (ClassNotFoundException e) {
-//            return false;
-//        }
+        try {
+            JavaFxUtils.class.getClassLoader().loadClass("javafx.embed.swing.JFXPanel");
+            return true;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
     }
 
 }
