7743cb5fc414630b44fb350286e4cdfdf39b55a2
Dan Moldoveanu <dan.moldoveanu@microfocus.com>
2023-05-15 16:42:11 +0300

    tech: opentext rebranding - adding new logo and preloader

--- src/main/java/com/hpe/adm/octane/ideplugins/intellij/ui/Constants.java
+++ src/main/java/com/hpe/adm/octane/ideplugins/intellij/ui/Constants.java
@@ -18,10 +18,12 @@ import com.intellij.util.ui.UIUtil;
 public class Constants {
 
     public static final String IMG_FOLDER_PATH = "/images/";
-    public static final String IMG_VENDOR_LOGO = IMG_FOLDER_PATH + "microfocus-blue-logo.png";
+    public static final String IMG_VENDOR_LOGO_LIGHT = IMG_FOLDER_PATH + "opentext-logo_light-theme-128.png";
+    public static final String IMG_VENDOR_LOGO_DARK = IMG_FOLDER_PATH + "opentext-logo_dark-theme-128.png";
     public static final String IMG_OCTANE_LOGO = IMG_FOLDER_PATH + "octane-logo.png";
     public static final String IMG_OCTANE_LOGO_20x20 = IMG_FOLDER_PATH + "octane-logo-20x20.png";
-    public static final String IMG_AJAX_SPINNER = IMG_FOLDER_PATH + "octane_preloader.gif";
+    public static final String IMG_SPINNER_LIGHT = IMG_FOLDER_PATH + "octane_preloader_light-128.gif";
+    public static final String IMG_SPINNER_DARK = IMG_FOLDER_PATH + "octane_preloader_dark-128.gif";
     public static final String IMG_REFRESH_ICON = IMG_FOLDER_PATH + "refresh-16x16.png";
     public static final String IMG_COMMENTS_ICON = IMG_FOLDER_PATH + "comments-16x16.png";
     public static final String IMG_BROWSER_ICON = IMG_FOLDER_PATH + "browser-16x16.png";
